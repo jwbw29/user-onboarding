10a5640c488ab473d94507a8e9af6d4f751c2755
Justin Byrd <justinbyrd7@gmail.com>
2023-08-23 11:19:27 -0500

    added submit handler

--- src/Form.js
+++ src/Form.js
@@ -1,8 +1,27 @@
+import * as Yup from "yup";
+/*
+# Checklist
+[ ] form UI
+[ ] submit handler
+[ ] yup schema
+[ ] apply yup validations
+*/
+
+// [ ] Yup schema goes here
+
 export default function Form() {
+  //[x] Create a submit event
+  const formSubmit = (evt) => {
+    evt.preventDefault();
+    console.log("submitted");
+  };
   return (
     <div className="bg-grey-lighter min-h-screen flex flex-col">
       <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-        <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+        <form
+          onSubmit={formSubmit}
+          className="bg-white px-6 py-8 rounded shadow-md text-black w-full"
+        >
           <h1 className="mb-8 text-3xl text-center">Sign up</h1>
           <input
             type="text"
@@ -33,8 +52,8 @@ export default function Form() {
 
           <label className="text-xs block w-full pb-3">
             {" "}
-            <input type="checkbox" name="consent" className="" /> Agree to the
-            Terms & Service Conditions
+            <input type="checkbox" name="consent" /> Agree to the Terms &
+            Service Conditions
           </label>
 
           <button
@@ -43,14 +62,7 @@ export default function Form() {
           >
             Create Account
           </button>
-
-          {/* <button
-            type="submit"
-            className="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
-          >
-            Create Account
-          </button> */}
-        </div>
+        </form>
       </div>
     </div>
   );
